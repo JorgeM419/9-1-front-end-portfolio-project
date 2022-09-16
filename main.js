029fec92bed55fc1e087e73a127a90c74e2e319c
JorgeM419 <jorgemoran@pursuit.org>
2022-09-16 17:10:12 -0400

    final

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
 const url =`https://db.ygoprodeck.com/api/v7/cardinfo.php`
+const element = document.querySelector('*');
+
+form.addEventListener("Check Price", (event) => {
+    event.preventDefault();
+
+    const { card } = event.target;
+  console.log(card);
+  getNewCardSearch(card.value);
+});
+
+let searchInfo = {};
+
+function getNewCardSearch (card) {
+    fetch(`${url}${card}?format=j1`)
+    .then((response) => response.json())
+    .then((json)=> {
+        console.log(`${json}`);
+
+        searchInfo["Card-name"] = json.data[0].name[0].value;
+        searchInfo["Card-set"] = json.data[1].setName[0].value;
+        getObjectData(searchInfo);
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+}
+
 
 
 fetch(url)
